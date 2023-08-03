4b07a1ccfe25797d9b8f5b31a6ee0f9f5dae0370
DGZCSXWD <fjjychenrui@gmail.com>
2023-08-03 14:46:30 +1000

    updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/react-quiz-app"
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/workout');
 
 module.exports = mongoose.connection;
