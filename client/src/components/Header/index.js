cd999b157607d320f72f3b9e14f31879da974587
DGZCSXWD <fjjychenrui@gmail.com>
2023-08-03 19:58:28 +1000

    update components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,51 +2,23 @@ import React from 'react';
 //import { Link } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Box } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Header = () => {
+  const navigate = useNavigate();
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
+    navigate("/")
   };
   return (
-    // <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-    //   <div className="container flex-row justify-space-between-lg justify-center align-center">
-    //     <div>
-    //       <Link className="text-light" to="/">
-    //         <h1 className="m-0">Workout Planner</h1>
-    //       </Link>
-    //       <p className="m-0">Shape Your Future, Start Today!</p>
-    //     </div>
-    //     <div>
-    //       {Auth.loggedIn() ? (
-    //         <>
-    //           <Link className="btn btn-lg btn-info m-2" to="/me">
-    //             {Auth.getProfile().data.username}'s Record
-    //           </Link>
-    //           <button className="btn btn-lg btn-light m-2" onClick={logout}>
-    //             Logout
-    //           </button>
-    //         </>
-    //       ) : (
-    //         <>
-    //           <Link className="btn btn-lg btn-info m-2" to="/login">
-    //             Login
-    //           </Link>
-    //           <Link className="btn btn-lg btn-light m-2" to="/signup">
-    //             Signup
-    //           </Link>
-    //         </>
-    //       )}
-    //     </div>
-    //   </div>
-    // </header>
     <AppBar position="static" color="primary">
       <Toolbar>
         <Box display="flex" flexGrow={1}>
           <RouterLink to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
             <Typography variant="h6">
-              Workout Planner
+              Workout Tracker
             </Typography>
             <Typography variant="subtitle1">
               Shape Your Future, Start Today!
--- client/src/components/WorkoutList/index.js
+++ client/src/components/WorkoutList/index.js
@@ -10,8 +10,6 @@ import {
 import UpvoteButton from "../UpvoteButton";
 import DownvoteButton from "../DownvoteButton";
 
-//import ReactionButton from "../ReactionButton";
-
 const WorkoutList = ({
   workouts,
   title,
@@ -54,7 +52,6 @@ const WorkoutList = ({
             <Button
               variant="contained"
               color="primary"
-              //fullWidth
               component={RouterLink}
               to={`/workouts/${workout._id}`}
             >
@@ -69,58 +66,3 @@ const WorkoutList = ({
 };
 
 export default WorkoutList;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const WorkoutList = ({
-//   workouts,
-//   title,
-//   showTitle = true,
-//   showUsername = true,
-// }) => {
-//   if (!workouts.length) {
-//     return <h3>No Workouts Yet</h3>;
-//   }
-
-//   return (
-//     <div>
-//       {showTitle && <h3>{title}</h3>}
-//       {workouts &&
-//         workouts.map((workout) => (
-//           <div key={workout._id} className="card mb-3">
-//             <h4 className="card-header bg-primary text-light p-2 m-0">
-//               {showUsername ? (
-//                 <Link
-//                   className="text-light"
-//                   to={`/profiles/${workout.workoutAuthor}`}
-//                 >
-//                   {workout.workoutAuthor} <br />
-//                   <span style={{ fontSize: '1rem' }}>
-//                     uploaded this workout record on {workout.createdAt}
-//                   </span>
-//                 </Link>
-//               ) : (
-//                 <>
-//                   <span style={{ fontSize: '1rem' }}>
-//                     You had this workout on {workout.createdAt}
-//                   </span>
-//                 </>
-//               )}
-//             </h4>
-//             <div className="card-body bg-light p-2">
-//               <p>{workout.workoutText}</p>
-//             </div>
-//             <Link
-//               className="btn btn-primary btn-block btn-squared"
-//               to={`/workouts/${workout._id}`}
-//             >
-//               Comment.
-//             </Link>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default WorkoutList;
